b32156cb748cd644d730ef48c9ec3abc430f3f80
JingfZhang <zhangjingfan.1996@gmail.com>
2019-06-20 19:00:50 +0000

    add to and remove from cart buttons added

--- public/scripts/menu_app.js
+++ public/scripts/menu_app.js
@@ -5,12 +5,19 @@ $(() => {
   }).done((items) => {
     for(item of items) {
       var $header = $("<header>");
-      var $p = $("<p>");
+      // var $p = $("<p>");
       var $footer = $("<footer>");
+      var $button = $(`<button type='submit' data-id='${item.id}' data-name='${item.name}'>`).addClass("add_to_cart");
+      var $buttonRemove = $(`<button type='submit' data-id='${item.id}' data-name='${item.name}'>`).addClass("remove");
+
 
       $header.append(item["name"]);
       // $p.append(item["description"]);
       $footer.append("$ " + item["price"]);
+      $button.append("ADD TO CART");
+      $buttonRemove.append("REMOVE");
+      $footer.append($button);
+      $footer.append($buttonRemove);
 
       $("#menu_body").append($header);
       // $("#menu_body").append($p);
@@ -18,4 +25,54 @@ $(() => {
 
     }
   });;
+
+  localStorage.clear();
+
+  $("#menu_body").on("click", ".add_to_cart", function (event) {
+    event.preventDefault()
+    var cart;
+    var itemId = $(event.target).data("id")
+    var itemName = $(event.target).data("name")
+    console.log(itemId)
+
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"))
+    } else {
+      cart = {}
+    }
+
+    if (cart[itemId]) {
+      cart[itemId].quantity += 1
+    } else {
+      cart[itemId] = {quantity: 1, name: itemName}
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart))
+    console.log(cart)
+  })
+
+    $("#menu_body").on("click", ".remove", function (event) {
+    event.preventDefault()
+    var cart;
+    var itemId = $(event.target).data("id")
+    var itemName = $(event.target).data("name")
+    console.log(itemId)
+
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"))
+    } else {
+      cart = {}
+    }
+
+    if (cart[itemId].quantity > 1) {
+      cart[itemId].quantity -= 1
+    } else {
+      delete cart[itemId];
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart))
+    console.log(cart)
+  })
+
 });
+
