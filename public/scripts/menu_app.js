82539162a727adc28ae4e7dc71f13a662ccc465e
asucarlos <kuwahara.mee@gmail.com>
2019-06-21 17:38:23 +0000

    Merge changes from master

--- public/scripts/menu_app.js
+++ public/scripts/menu_app.js
@@ -7,9 +7,15 @@ $(() => {
       var $header = $("<header>");
       // var $p = $("<p>");
       var $footer = $("<footer>");
+<<<<<<< Updated upstream
       var $button = $(`<button type='submit' data-id='${item.id}' data-name='${item.name}'>`).addClass("add_to_cart");
       var $buttonRemove = $(`<button type='submit' data-id='${item.id}' data-name='${item.name}'>`).addClass("remove");
 
+=======
+      var $button = $(`<button type='submit' data-id='${item.id}' data-name='${item.name}' data-price='${item.price}'>`).addClass("add");
+      var $buttonRemove = $(`<button type='submit' data-id='${item.id}' data-name='${item.name}' data-price='${item.price}'>`).addClass("remove");
+      var $counter = $(`<span id='${item.id}' data-id='${item.id}' data-name='${item.name}' data-price='${item.price}'>`).addClass("item_counter");
+>>>>>>> Stashed changes
 
       $header.append(item["name"]);
       // $p.append(item["description"]);
@@ -18,6 +24,8 @@ $(() => {
       $buttonRemove.append("REMOVE");
       $footer.append($button);
       $footer.append($buttonRemove);
+      $footer.append($counter);
+      $(".item_counter").html(0);
 
       $("#menu_body").append($header);
       // $("#menu_body").append($p);
@@ -26,7 +34,51 @@ $(() => {
     }
   });;
 
+<<<<<<< Updated upstream
   localStorage.clear();
+=======
+  $("section.pizza").slideUp(1);
+  $("section.pasta").slideUp(1);
+  $("section.burger").slideUp(1);
+  $("section.wings").slideUp(1);
+  $("section.pop").slideUp(1);
+
+  $("button.pizza").click(function() {
+    $("section.pizza").slideToggle();
+    $("section.pasta").slideUp(1);
+    $("section.burger").slideUp(1);
+    $("section.wings").slideUp(1);
+    $("section.pop").slideUp(1);
+  })
+  $("button.pasta").click(function() {
+    $("section.pasta").slideToggle();
+    $("section.pizza").slideUp(1);
+    $("section.burger").slideUp(1);
+    $("section.wings").slideUp(1);
+    $("section.pop").slideUp(1);
+  })
+  $("button.burger").click(function() {
+    $("section.burger").slideToggle();
+    $("section.pasta").slideUp(1);
+    $("section.pizza").slideUp(1);
+    $("section.wings").slideUp(1);
+    $("section.pop").slideUp(1);
+  })
+  $("button.wings").click(function() {
+    $("section.wings").slideToggle();
+    $("section.pasta").slideUp(1);
+    $("section.burger").slideUp(1);
+    $("section.pizza").slideUp(1);
+    $("section.pop").slideUp(1);
+  })
+  $("button.pop").click(function() {
+    $("section.pop").slideToggle();
+    $("section.pasta").slideUp(1);
+    $("section.burger").slideUp(1);
+    $("section.wings").slideUp(1);
+    $("section.pizza").slideUp(1);
+  })
+>>>>>>> Stashed changes
 
   $("#menu_body").on("click", ".add_to_cart", function (event) {
     event.preventDefault()
@@ -47,6 +99,7 @@ $(() => {
       cart[itemId] = {quantity: 1, name: itemName}
     }
 
+    $(`span.item_counter#${itemId}`).html(cart[itemId].quantity);
     localStorage.setItem("cart", JSON.stringify(cart))
     console.log(cart)
   })
@@ -66,8 +119,14 @@ $(() => {
 
     if (cart[itemId].quantity > 1) {
       cart[itemId].quantity -= 1
+<<<<<<< Updated upstream
+=======
+      cart[itemId].price = (cart[itemId].quantity * itemPrice).toFixed(2);
+      $(`span.item_counter#${itemId}`).html(cart[itemId].quantity);
+>>>>>>> Stashed changes
     } else {
       delete cart[itemId];
+      $(`span.item_counter#${itemId}`).html(0);
     }
 
     localStorage.setItem("cart", JSON.stringify(cart))
@@ -75,4 +134,3 @@ $(() => {
   })
 
 });
-
