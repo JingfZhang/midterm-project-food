148fca364f96d8c6e34aed2667b4d4911e49acf1
asucarlos <42548220+asucarlos@users.noreply.github.com>
2019-06-23 14:52:03 -0400

    Featurelastapages layout2 (#27)
    * added twilio folder to gitignore page
    
    * edited appearance of buttons
    
    * fix calculation bug
    
    * change UI for last pages

--- public/scripts/cart_app.js
+++ public/scripts/cart_app.js
@@ -11,12 +11,11 @@ $(() => {
                     <span class = "item-name">${cartItem.name}</span>
                     <span class = "item-quantity">${cartItem.quantity}</span>
                 </header>
-                <footer class="remove cart-item-footer">
-                  <button class ="btn btn-primary cart-remove-button" type='submit' data-id='${item}' data-name='${cartItem.name}'>
-                    REMOVE
-                  </button>
-                  <span class = "item-price">$${cartItem.price}
-                </footer>
+                <footer class="cart-item-footer">
+                 <span class = "item-price">$${cartItem.price}</span>
+                 <i class ="far fa-trash-alt btn btn-primary cart-remove-button" type='submit' data-id='${item}' data-name='${cartItem.name}'>
+                 </i>
+                 </footer>
               </div>
           `;
           $("#cart-body").append($container);
@@ -30,12 +29,12 @@ $(() => {
     //takes remove/orderclear function as cb
     populateCart( () => {
       //remove selected items in the cart 
-      $("#cart-body .remove").on("click", function (event) {
+      $("#cart-body .cart-remove-button").on("click", function (event) {
         event.preventDefault();
        var cart = JSON.parse(localStorage.getItem("cart"))
        var itemId = $(event.target).data("id");
-    
-        $(this).parent().remove();
+        $(this).parent().parent().remove();
+
         $total = $total - parseFloat(cart[itemId].price)
         $total = round($total);
 
